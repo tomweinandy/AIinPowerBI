9caca0c039e9b6b563fdc521599218cc706cecb3
Thomas Weinandy <tomweinandy@gmail.com>
2023-07-10 00:01:51 -0400

    Fix typo in Python.  Begin building out examples in R

--- google_trends_examples.R
+++ google_trends_examples.R
@@ -1,1 +1,18 @@
-print('hello world')
+# Set directory
+filepath <- "/Users/mymac/Downloads/"
+# filepath = "C:/Users/tomwe/Documents/GoogleTrendsData/"
+setwd(filepath)
+
+# Read the list of files in the specified folder
+file_list <- list.files()
+
+# Extract "multiTimeline" files
+filtered_list <- grep("^multiTimeline", file_list, value=TRUE)
+
+# Identify the most recent date
+most_recent_file <- max(filtered_list)
+
+# Load data
+full_filepath <- paste(filepath, most_recent_file, sep="")
+df <- read.csv(most_recent_file, skip=2)
+print(df)
