90d9a285ae057804b51c175fabfacc20626a71cd
Thomas Weinandy <tomweinandy@gmail.com>
2023-07-15 10:03:52 -0400

    Add R code for data ingest, data transformation, and data visualization

--- google_trends_examples.R
+++ google_trends_examples.R
@@ -1,3 +1,4 @@
+##### Ingest data
 # Set directory
 filepath <- "/Users/mymac/Downloads/"
 # filepath = "C:/Users/tomwe/Documents/GoogleTrendsData/"
@@ -16,3 +17,41 @@ most_recent_file <- max(filtered_list)
 full_filepath <- paste(filepath, most_recent_file, sep="")
 df <- read.csv(most_recent_file, skip=2)
 print(df)
+
+
+### Transform data
+# Import packages
+# install.packages("zoo")
+library(zoo)
+
+# Read in data
+df = read.csv(url("https://raw.githubusercontent.com/tomweinandy/AIinPowerBI/main/multiTimeline2023-06.csv"), skip=2)
+df_rolling = df
+
+# Rename columns
+colnames(df_rolling)[colnames(df_rolling) == "python.machine.learning...United.States."] <- "python machine learning"
+colnames(df_rolling)[colnames(df_rolling) == "r.machine.learning...United.States."] <- "r machine learning"
+
+# Add six-month moving average
+df_rolling$Month <- as.Date(paste(df_rolling$Month, "01", sep = "-"))
+df_rolling$"python moving average" <- rollapply(df_rolling$"python machine learning", width=6, FUN=mean, align="right", partial=TRUE)
+df_rolling$"r moving average" <- rollapply(df_rolling$"r machine learning", width=6, FUN=mean, align="right", partial=TRUE)
+
+# Load data
+print(df_rolling)
+
+
+### Visualize data
+# Import packages
+
+# Format plot and add monthly observations as a scatter plot
+plot(df_rolling$Month, df_rolling$"python machine learning", pch=16, col=rgb(1, 0.85, 0, alpha=0.5), xlab="", ylab="Google Search Frequency Index")
+points(df_rolling$Month, df_rolling$"r machine learning", col=rgb(1, 0, 0, alpha=0.5), pch=16)
+
+# Plot moving averages as a line
+lines(df_rolling$Month, df_rolling$"python moving average", col=rgb(1, 0.85, 0), lwd=3)
+lines(df_rolling$Month, df_rolling$"r moving average", col="red", lwd=3)
+
+# Add the legend and title
+legend("topleft", legend=c("python machine learning", "r machine learning"), col=c(rgb(1, 0.85, 0), "red"), lwd=c(2, 2), bty="n")
+title(main="Change in Programming Language Popularity Over Time")
